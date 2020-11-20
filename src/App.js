0d8520e0216b4f70007e247c21853d1e78b961eb
Phillip Tran <pntran@ucdavis.edu>
2020-11-20 15:19:12 -0800

    testing our pipeline

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Also testing the auto builder
+          Testing our pipeline!!
         </p>
         <a
           className="App-link"
